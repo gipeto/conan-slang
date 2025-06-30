0e301dcfc9c5355fe33497127b186bc88cc4c6a5
Javier Perez <202426+gipeto@users.noreply.github.com>
2025-06-30 23:55:29 +0200

    Update version to 2025.11 and fix windows test package (#2)
    * Bump to latest version of slang
    
    * Fix failing test_package on Windows
    
    * Use cmake package info packaged with the library instead
      of a conan generated one to keep consumer code conan
      agnostic

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,6 +3,8 @@ import os
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.build import can_run
+from conan.tools.files import copy
+
 
 class slangTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -19,7 +21,16 @@ class slangTestConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def generate(self):
+        for dep in self.dependencies.values():
+            copy(
+                self,
+                "*.dll",
+                dep.cpp_info.bindirs[0],
+                os.path.join(self.build_folder, self.cpp.build.bindir),
+            )
+
     def test(self):
         if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindir, "example")
+            cmd = os.path.join(self.cpp.build.bindir, "testPackageApp")
             self.run(cmd, env="conanrun")
