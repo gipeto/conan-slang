e3919e9a9ca28d73855479213885d726a834bf9c
gipeto <202426+gipeto@users.noreply.github.com>
2025-07-02 23:05:37 +0200

    Remove pinned version

--- test_package/src/testPackageApp.cpp
+++ test_package/src/testPackageApp.cpp
@@ -7,13 +7,13 @@ int main()
 
   if (const auto result = createGlobalSession(&outGlobalSession);
       result >= 0 && outGlobalSession != nullptr) {
-    std::cout << "[Success] Test package could create global session"
+    std::cout << "[Success] Test package could create a global session"
               << std::endl;
     outGlobalSession->release();
     return 0;
   }
 
-  std::cout << "[Failure] Test package could create global session"
+  std::cout << "[Failure] Test package could not create a global session"
             << std::endl;
   return -1;
 }
